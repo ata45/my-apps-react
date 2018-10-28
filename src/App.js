4e41c34172570699440d7070ae7b6eb43d293e61
ata45 <34927815+ata45@users.noreply.github.com>
2018-10-28 08:10:25 +0700

    Coba

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      /*<div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -20,7 +20,10 @@ class App extends Component {
             Learn React
           </a>
         </header>
-      </div>
+      </div>*/
+      
+        <h1> Ini header </h1>
+        <p data-myatribute = "someatribute"> </p>
     );
   }
 }
