01c6f3b248b27fb5a060d0978ba04679cb8f353a
ata45 <34927815+ata45@users.noreply.github.com>
2018-10-30 23:25:50 +0700

    Update src/state.js

---
+++ src/state.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+
+class state extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      header:"ini header"
+    }
+  }
+  render(){
+    rerturn(
+      <div>
+        
+      </div>
+    );
+  }
+}
